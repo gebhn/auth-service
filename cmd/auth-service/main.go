ae6e12872c33e83ee96cd1c6fc321a2ad29d367c
Nicholas Gebhart <mail@gebhn.com>
2025-06-01 22:18:33 -0700

    feat: migrates database

--- cmd/auth-service/main.go
+++ cmd/auth-service/main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/gebhn/auth-service/internal/config"
+	"github.com/gebhn/auth-service/internal/db"
+	"github.com/golang-migrate/migrate/v4"
 )
 
 func main() {
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
 
+	c := db.NewLibsqlConn(config.GetTursoDbUrl(), config.GetTursoDbToken())
+	m := db.NewMigrator(c)
+
 	go func() {
-		log.Println("Hello, world")
+		if err := m.Down(); err != nil {
+			if !errors.Is(err, migrate.ErrNoChange) {
+				log.Fatal(err)
+			}
+		}
+		if err := m.Up(); err != nil {
+			if !errors.Is(err, migrate.ErrNoChange) {
+				log.Fatal(err)
+			}
+		}
 	}()
 
 	<-stop
